de155cabbfd71b683ab66fcdfe331ffafbd67bb1
EUNBI203 <unbi203@gmail.com>
2019-08-19 16:40:09 +0900

    04 | articles - CRUD - C

--- articles/views.py
+++ articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Article
 # Create your views here.
@@ -8,3 +8,17 @@ def index(request):
         'articles' : articles
     }
     return render(request, 'articles/index.html', context)
+
+def new(request):
+    return render(request, 'articles/new.html')
+
+def create(request):
+    new_title = request.GET.get('title')
+    new_content = request.GET.get('content')
+    article = Article(title=new_title, content=new_content)
+    article.save()
+    context = {
+        'article': article
+    }
+    # return render(request, 'articles/create.html', context)
+    return redirect('/articles/')
