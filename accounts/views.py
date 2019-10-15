f4eab6556f9e2a51a66e353bcd6bae251e425ce3
EUNBI203 <unbi203@gmail.com>
2019-10-15 14:09:26 +0900

    26 | Accounts login

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,9 @@
 # from IPython import embed
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST, require_GET
-from django.contrib.auth.forms import UserCreationForm
-# from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login as auth_login
+
 # Create your views here.
 def signup(request):
     if request.method == 'POST':
@@ -16,3 +17,17 @@ def signup(request):
         'user_form': user_form
     }
     return render(request, 'accounts/signup.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'Form': form
+    }
+    return render(request, 'accounts/login.html', context)
